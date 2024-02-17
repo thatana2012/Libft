783347a06c7072ef9fd2f8082425dd2d1f11e988
thatana2012 <149284730+thatana2012@users.noreply.github.com>
2024-02-17 23:19:16 +0700

    add split

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: tjerdnap <tjerdnap@student.42bangkok.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 16:40:35 by tjerdnap          #+#    #+#             */
-/*   Updated: 2024/02/17 17:39:52 by tjerdnap         ###   ########.fr       */
+/*   Updated: 2024/02/17 22:20:35 by tjerdnap         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,9 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 
 	temp_dest = (unsigned char *)dest;
 	temp_src = (unsigned char *)src;
-	if (temp_dest < temp_src)
+	len = n;
+	if (temp_dest > temp_src && temp_dest < temp_src + len)
 	{
-		len = n;
 		while (len-- > 0)
 			temp_dest[len] = temp_src[len];
 	}
@@ -43,14 +43,14 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 
 int	main(void)
 {
-	char src[100] = "Hello12345";
+	char src[100] = "He12345";
 	char *first, *second;
 	first = src;
 	second = src;
 	printf("Ori: %s\n", src);
-	memcpy(first + 2, first, 5);
-	printf("memcpy overlap: %s\n", src);
-	ft_memmove(second + 2, first, 5);
+	memcpy(first + 4, first, 8);
+	printf("memcpy overlap:  %s\n", src);
+	ft_memmove(second + 4, first, 8);
 	printf("memmove overlap: %s\n", src);
 	return (0);
 }*/
